b9a92007ec92e1177effadcbb7298bc72368e5e2
chr0927 <769083967@qq.com>
2017-07-13 10:49:20 +0800

    这是一个组作品

--- 组作品/js/homepage.js
+++
@@ -1,9 +0,0 @@
-
-var hb = document.getElementsByClassName('headerHobby')[0];
-var hp = document.getElementsByClassName('homepage')[0];
-hb.onclick = function(){
-	open('hobby.html','_self')
-}
-hp.onclick = function(){
-	open('home.html','_self')
-}
